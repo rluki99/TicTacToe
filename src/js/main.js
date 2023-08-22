9502a81eb44f9973bf9a89e0119c4d0944940115
rluki99 <lukasz.beczek24@gmail.com>
2023-08-22 12:47:51 +0200

    feat: player vs player

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-const gameBoard = () => {
+const gameBoard = (() => {
 	const board = Array(9).fill('')
 
 	const getBoard = () => board
@@ -11,12 +11,12 @@ const gameBoard = () => {
 		return false
 	}
 
-	const isSpotTaken = (index) => board[index] !== ''
+	const isSpotTaken = index => board[index] !== ''
 
-	const isBoardFull = () => board.every((cell) => cell !== '')
+	const isBoardFull = () => board.every(cell => cell !== '')
 
 	return { getBoard, makeMove, isSpotTaken, isBoardFull }
-}
+})()
 
 const createPlayer = (name, symbol) => {
 	return {
@@ -26,10 +26,76 @@ const createPlayer = (name, symbol) => {
 }
 
 const gameController = () => {
-    const player1 = createPlayer('Player 1', 'X')
-    const player2 = createPlayer('Player 2', 'O')
-    let currentPlayer = player1
-    let gameActive = false
+	const player1 = createPlayer('Player 1', 'X')
+	const player2 = createPlayer('Player 2', 'O')
+	let currentPlayer = player1
+	let gameActive = false
 
-    const cells = document.querySelectorAll('.board__cell')
+	const cells = document.querySelectorAll('.board__cell')
+
+	const startGame = () => {
+		gameActive = true
+		cells.forEach(cell => {
+			cell.textContent = ''
+		})
+		cells.forEach((cell, index) => {
+			cell.addEventListener('click', () => {
+				if (gameActive && gameBoard.makeMove(index, currentPlayer.symbol)) {
+					cell.textContent = currentPlayer.symbol
+					if (checkWinner()) {
+						endGame(currentPlayer)
+					} else if (checkTie()) {
+						endGame(null)
+					} else {
+						currentPlayer = currentPlayer === player1 ? player2 : player1
+					}
+				}
+			})
+		})
+	}
+
+	const endGame = winner => {
+		gameActive = false
+		if (winner) {
+			console.log(`${winner.name} wins!`)
+		} else {
+			console.log(`It's a tie!`)
+		}
+	}
+
+	const checkWinner = () => {
+		const winningCombos = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8], // Wiersze
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8], // Kolumny
+			[0, 4, 8],
+			[2, 4, 6], // Przekątne
+		]
+
+		return winningCombos.some(combo => {
+			const [a, b, c] = combo
+			return (
+				gameBoard.getBoard()[a] !== '' &&
+				gameBoard.getBoard()[a] === gameBoard.getBoard()[b] &&
+				gameBoard.getBoard()[a] === gameBoard.getBoard()[c]
+			)
+		})
+	}
+
+	const checkTie = () => {
+		return gameBoard.isBoardFull() && !checkWinner()
+	}
+
+	cells.forEach(cell => {
+		cell.addEventListener('click', () => {
+			if (!gameActive) {
+				startGame()
+			}
+		})
+	})
 }
+
+gameController()
